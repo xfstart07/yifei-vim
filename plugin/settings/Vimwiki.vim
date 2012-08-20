789a0d98f5f380cef39143bea6706756f5e073bf
Yifei Xu <xfstart07@gmail.com>
2012-08-20 18:41:02 +0800

    renew yifei-vim

---
+++ plugin/settings/Vimwiki.vim
@@ -0,0 +1,2 @@
+let g:vimwiki_camel_case = 0
+let g:vimwiki_list = [{'path': '~/dropbox/note', 'syntax': 'markdown', 'path_html': '/media/B/Dropbox/Work/note/html/'}]
---
+++ submoudle.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+git submodule add git://github.com/vim-scripts/matchit.zip.git bundle/matchit
+git submodule add git://github.com/scrooloose/nerdcommenter.git bundle/nerdcommenter
+git submodule add git://github.com/scrooloose/nerdtree.git bundle/nerdtree
+git submodule add git://github.com/jistr/vim-nerdtree-tabs.git bundle/vim-nerdtree-tabs
+git submodule add git://github.com/msanders/snipmate.vim.git bundle/snipmate
+git submodule add git://github.com/ervandew/supertab.git bundle/supertab
+git submodule add git://github.com/vim-scripts/taglist.vim.git bundle/taglist.vim
+git submodule add git://github.com/ap/vim-css-color.git bundle/vim-css-color
+git submodule add git://github.com/tpope/vim-pathogen.git bundle/vim-pathogen
+git submodule add git://github.com/tpope/vim-rails.git bundle/vim-rails
+git submodule add git://github.com/tpope/vim-repeat.git bundle/vim-repeat
+git submodule add git://github.com/vim-ruby/vim-ruby.git bundle/vim-ruby
+git submodule add git://github.com/tpope/vim-surround.git bundle/vim-surround
+git submodule add git://github.com/Lokaltog/vim-powerline.git bundle/vim-powerline
+git submodule add git://github.com/cakebaker/scss-syntax.vim.git bundle/vim-scss
+git submodule add git://github.com/tpope/vim-haml.git bundle/vim-sass
+git submodule add git://github.com/sunaku/vim-ruby-minitest.git bundle/vim-minitest
+git submodule add git://github.com/pangloss/vim-javascript.git bundle/vim-javascript
+git submodule add git://github.com/tangledhelix/vim-octopress.git bundle/vim-octopress
+git submodule add git://github.com/michaeljsmith/vim-indent-object.git bundle/vim-indent-object
+git submodule add git://github.com/digitaltoad/vim-jade.git bundle/vim-jade
+git submodule add git://github.com/vim-scripts/vimwiki.git bundle/vimwiki
+git submodule add git://github.com/plasticboy/vim-markdown.git bundle/vim-markdown
+git submodule add git://github.com/Townk/vim-autoclose.git bundle/autoclose
+
